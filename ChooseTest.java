b7418ab2189e54dc255e5eebb31a2c2059112fc9
Howard809 <65829332+Howard809@users.noreply.github.com>
2025-04-10 16:31:09 -0700

    Update ChooseTest.java

--- ChooseTest.java
+++ ChooseTest.java
@@ -1,4 +1,8 @@
-package com.example.project;
+//INFO 3245 - Course Project ChooseTest.java
+//Blood Test Booking App with Firebase and Recycler View
+//Asmaa Almasri - 100350706
+//Howard Chen - 100382934
+package com.example.courseproject;
 
 import android.os.Bundle;
 
