d59f06c21c5d00da5b7d04dcd59a6fd6ba088719
Howard809 <65829332+Howard809@users.noreply.github.com>
2025-04-11 00:18:51 -0700

    Update TimePickerDialog.java

--- TimePickerDialog.java
+++ TimePickerDialog.java
@@ -3,7 +3,7 @@
 //Asmaa Almasri - 100350706
 //Howard Chen - 100382934
 
-package com.example.courseproject;
+package com.example.newproject;
 
 public class TimePickerDialog {
     public TimePickerDialog(Booking1 booking1, Object o, int hour, int minute, boolean b) {
